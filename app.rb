4760ab9253067c4da6acd7db2dce73245316d11c
Jeff Seymour <seym1585@gmail.com>
2016-01-29 10:27:28 -0800

    add functionality to add a store

--- app.rb
+++ app.rb
@@ -2,3 +2,26 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get('/') do
+  @brands = Brand.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+get('/stores/new') do
+  erb(:store_form)
+end
+
+get('/stores/:id') do
+  @store = Store.find(params[:id])
+  @brands = @store.brands
+  erb(:store)
+end
+
+post('/') do
+  store_name = params.fetch('store_name')
+  store = Store.create({name: store_name})
+  redirect("/")
+end
---
+++ spec/store_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature "Adding a store" do
+  scenario "allows the user to add a recipe" do
+    visit('/')
+    click_link 'Add a New Store'
+    fill_in 'store_name', with: 'test store'
+    click_button 'Submit'
+    expect(page).to have_content('test store')
+  end
+end
